6cd8685b220bcdbd2145bf4aa9962b44e3a6d0b6
mohdbilal <mohdbilal3386@gmail.com>
2023-01-14 22:43:39 +0530

    request/response

--- node_backend/app.js
+++ node_backend/app.js
@@ -1,7 +1,35 @@
-const http = require('http')
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-   console.log("MOHD BILAL")
-})
+  const url = req.url;
+  res.setHeader("Content-Type", "text/html");
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
+  if (url === "/home") {
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1>Welcome Home!</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
 
-server.listen(3000)
+  if (url === "/about") {
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1>Welcome to  About Us page</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
+  
+});
+
+server.listen(3000);
+
+/*
+console.log(req.url, req.method, req.headers)
+*/
